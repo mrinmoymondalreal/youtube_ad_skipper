a53adf23b689c4fed3a04e052e1791e2ad4c1a66
Mrinmoy Mondal <mmtech6100@gmail.com>
2022-07-07 20:28:09 +0530

    updated code

--- background.js
+++ background.js
@@ -5,12 +5,10 @@ function logKey(e) {
         if(document.querySelector(".ytp-ad-skip-button")) document.querySelector(".ytp-ad-skip-button").click();
         if(document.querySelector('a[class="ytp-title-link yt-uix-sessionlink ytp-title-fullerscreen-link"]').innerHTML){
             if(document.querySelector('a[class="ytp-title-link yt-uix-sessionlink ytp-title-fullerscreen-link"]').innerHTML !== document.querySelector('h1 yt-formatted-string[class="style-scope ytd-watch-metadata"]').innerHTML){
-                if(document.querySelector("video")){
-                    document.querySelector("video").currentTime = document.querySelector("video").duration;
-                    if(document.querySelector(".ytp-ad-skip-button")) document.querySelector(".ytp-ad-skip-button").click();
-                }
-                console.log("skipped");
-                document.querySelectorAll('video[tabindex="-1"]')[1].currentTime = document.querySelectorAll('video[tabindex="-1"]')[1].duration;
+                var f;
+                document.querySelectorAll("video").forEach(e=>{if(e.currentTime > 0) f = e;});
+                f.currentTime = f.duration;
+                document.querySelector(".ytp-ad-skip-button").click();
             }
         }
     }
