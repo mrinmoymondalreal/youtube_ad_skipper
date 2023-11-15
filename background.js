b3a87927bffe44b0564c218c121e906c7d0c0996
Mrinmoy Mondal <mrinmoymondalreal@gmail.com>
2023-11-15 13:44:17 +0530

    Improved & Updated

--- background.js
+++ background.js
@@ -2,13 +2,13 @@ document.addEventListener('keydown', logKey);
 
 function logKey(e) {
     if(e.keyCode==83){
-        if(document.querySelector(".ytp-ad-skip-button")) document.querySelector(".ytp-ad-skip-button").click();
+        if(document.querySelector(`button[class="ytp-ad-skip-button-modern ytp-button"]`)) document.querySelector(`button[class="ytp-ad-skip-button-modern ytp-button"]`).click();
         if(document.querySelector('a[class="ytp-title-link yt-uix-sessionlink ytp-title-fullerscreen-link"]').innerHTML){
-            if(document.querySelector('a[class="ytp-title-link yt-uix-sessionlink ytp-title-fullerscreen-link"]').innerHTML !== document.querySelector('h1 yt-formatted-string[class="style-scope ytd-watch-metadata"]').innerHTML){
+            while(document.querySelector('a[class="ytp-title-link yt-uix-sessionlink ytp-title-fullerscreen-link"]').innerHTML !== document.querySelector('h1 yt-formatted-string[class="style-scope ytd-watch-metadata"]').innerHTML){
                 var f;
                 document.querySelectorAll("video").forEach(e=>{if(e.currentTime > 0) f = e;});
                 f.currentTime = f.duration;
-                document.querySelector(".ytp-ad-skip-button").click();
+                document.querySelector(`button[class="ytp-ad-skip-button-modern ytp-button"]`).click();
             }
         }
     }
